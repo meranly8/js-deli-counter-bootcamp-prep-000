5acca67809f708488139248016a160ff90e813fc
Michelle Ranly <meranly8@gmail.com>
2018-01-16 17:00:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,9 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine) {
   let l = katzDeliLine.length
   
+  if (l === 0) {
+    return 'The line is currently empty.'
+  } else if {
+    
+  }
 }
