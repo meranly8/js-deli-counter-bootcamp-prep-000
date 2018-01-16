4eeb34fedab034736375b633102b6a2c41ada74f
Michelle Ranly <meranly8@gmail.com>
2018-01-16 16:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length < 0) {
     var next = `Currently serving ${katzDeliLine[0]}`
     colsole.log(next)
-  }
-  return katzDeli[0]
+  
   katzDeli.shift()
+  } else {
+    
+  }
+  
 }
