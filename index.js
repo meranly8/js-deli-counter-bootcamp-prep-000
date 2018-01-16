7e6d0efb8058ce726d05ffd5d6b7d6b9fec0005d
Michelle Ranly <meranly8@gmail.com>
2018-01-16 16:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   return `Welcome ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(){
+  return katzDeli[0]
+  katzDeli.shift()
+}
